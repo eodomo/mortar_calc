b16fe0df6bc231badbaf57b3277291e24ee073a3
Eric Olerud <eric@olerud.com>
2026-09-20 09:24:37 -0400

    fix angle not matching reality

--- point.go
+++ point.go
@@ -13,7 +13,14 @@ func GetDistance(p1 Point, p2 Point) float64 {
 	return math.Pow(x2+y2, (0.5)) * 100.0
 }
 
-func GetAngle(p1 Point, p2 Point) float64 {
-	angle := math.Atan((p2.y-p1.y)/(p2.x-p1.x)) * (180 / math.Pi)
-	return math.Mod(math.Mod(angle, 360.0)+360.0, 360.0)
+func GetAngle(p1, p2 Point) float64 {
+	dx := p2.x - p1.x
+	dy := p2.y - p1.y
+
+	// Atan2(dx, -dy) converts Cartesian/screen coordinates
+	// into a clockwise compass bearing from north.
+	angle := math.Atan2(dx, -dy) * 180 / math.Pi
+
+	// Normalize [-180, 180] to [0, 360).
+	return math.Mod(angle+360, 360)
 }
